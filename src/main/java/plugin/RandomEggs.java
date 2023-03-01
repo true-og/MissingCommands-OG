bca22a8f1bde0457e141334a411d927a419529f5
s0me0ne-coder <72974445+s0me0ne-coder@users.noreply.github.com>
2023-03-01 20:45:52 +0300

    Update RandomEggs.java

--- src/main/java/plugin/RandomEggs.java
+++ src/main/java/plugin/RandomEggs.java
@@ -6,16 +6,15 @@ package plugin;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-import io.papermc.paper.event.block.DragonEggFormEvent
+import io.papermc.paper.event.block.DragonEggFormEvent;
 import java.util.Random;
 
 // Extending this class is standard bukkit boilerplate for any plugin, or else the server software won't load the classes.
 public class RandomEggs extends JavaPlugin {
 	bool isRunning = false;
 	
-	@EventHandler
+	@EventHandler(priority = EventPriority.HIGHEST)
 	public class DragonEggFormEvent extends RandomEggs {
-		event.EventPriority = HIGHEST;
 		// 10% chance
 		if (this.isRunning && Random.nextDouble < 0.1) event.setCanceled = false;
 		else event.setCanceled = true;
