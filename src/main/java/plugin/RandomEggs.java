ce1d9320d07908b057aad53844b528249b8905f7
s0me0ne-coder <72974445+s0me0ne-coder@users.noreply.github.com>
2023-03-01 21:47:21 +0300

    Update RandomEggs.java

--- src/main/java/plugin/RandomEggs.java
+++ src/main/java/plugin/RandomEggs.java
@@ -7,29 +7,16 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import io.papermc.paper.event.block.DragonEggFormEvent;
-import java.util.Random;
+
 
 // Extending this class is standard bukkit boilerplate for any plugin, or else the server software won't load the classes.
-public class RandomEggs extends JavaPlugin implements EventListener{
+public class RandomEggs extends JavaPlugin{
 	boolean isRunning = false;
-	Random r = new Random();
-	@EventHandler(priority = EventPriority.HIGHEST)
-	public void onDragonEggDrop(DragonEggFormEvent event) {
-		// 10% chance
-		if (isRunning && r.nextDouble() < 0.1) {event.setCancelled(false);}
-		else {event.setCancelled(true);}
-	}
 	
 	// What to do when the plugin is run by the server.
 	public void onEnable() {
-		isRunning = true;
 		// register the event
 		getServer().getPluginManager().registerEvents(new RandomEggs(), this);
-	}
-
-	// What to do when the plugin is disabled by the server (such as during a reboot).
-	public void onDisable() {
-		isRunning = false;
 	}
 
 }
