963f245ed2af4187cc6556826abbd86c0b845490
s0me0ne-coder <72974445+s0me0ne-coder@users.noreply.github.com>
2023-03-01 20:21:28 +0300

    Update and rename NoClip.java to RandomEggs.java

---
+++ src/main/java/plugin/RandomEggs.java
@@ -0,0 +1,35 @@
+// This is free and unencumbered software released into the public domain.
+// Author: NotAlexNoyle (admin@true-og.net)
+
+package plugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+import io.papermc.paper.event.block.DragonEggFormEvent
+
+
+// Extending this class is standard bukkit boilerplate for any plugin, or else the server software won't load the classes.
+public class RandomEggs extends JavaPlugin {
+	bool isRunning = false;
+	
+	@EventHandler
+	public class DragonEggFormEvent extends RandomEggs {
+        // 10% chance
+	// if (this.isRunning && )
+}
+	
+	// What to do when the plugin is run by the server.
+	public void onEnable() {
+		isRunning = true;
+
+		// Register the instantiation of the plugin (triggers loading with messages in console).
+		getServer().getPluginManager().registerEvents(new Listeners(), (Plugin) this);
+	}
+
+	// What to do when the plugin is disabled by the server (such as during a reboot).
+	public void onDisable() {
+		isRunning = false;
+	}
+
+}
