31b480ae77344b5b9b830a761589f50ed9561e2e
s0me0ne-coder <72974445+s0me0ne-coder@users.noreply.github.com>
2023-03-01 21:49:32 +0300

    Update RandomEggs.java

--- src/main/java/plugin/RandomEggs.java
+++ src/main/java/plugin/RandomEggs.java
@@ -11,7 +11,6 @@ import io.papermc.paper.event.block.DragonEggFormEvent;
 
 // Extending this class is standard bukkit boilerplate for any plugin, or else the server software won't load the classes.
 public class RandomEggs extends JavaPlugin{
-	boolean isRunning = false;
 	
 	// What to do when the plugin is run by the server.
 	public void onEnable() {
