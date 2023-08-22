b1521da515f7e9b0e2e048f6b354a772d38d6d3f
NotAlexNoyle <alexnoyle@icloud.com>
2023-08-21 21:29:14 -0400

    Add Utilities-OG API call example

--- src/main/java/plugin/Listeners.java
+++ src/main/java/plugin/Listeners.java
@@ -2,17 +2,21 @@
 // Author: NotAlexNoyle.
 package plugin;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+import me.barny1094875.utilitiesog.Utilities_OG;
 
 public class Listeners implements Listener {
-  
-	//@EventHandler(priority = EventPriority.HIGHEST)
-	//public void onPlayerMove(PlayerMoveEvent event) {
-	
 
-	
-	//}
-  
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onPlayerMove(PlayerMoveEvent event) {
+
+		Utilities_OG utilities = (Utilities_OG) Bukkit.getPluginManager().getPlugin("Utilities-OG");
+
+	}
+
 }
--- src/main/java/plugin/TemplateOG.java
+++ src/main/java/plugin/TemplateOG.java
@@ -7,10 +7,10 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 // Extending this class is standard bukkit boilerplate for any plugin, or else the server software won't load the classes.
 public class TemplateOG extends JavaPlugin {
-	
+
 	// What to do when the plugin is run by the server.
 	public void onEnable() {
-		
+
 		// Register the event.
 		getServer().getPluginManager().registerEvents(new Listeners(), this);
 
